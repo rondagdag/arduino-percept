ee70ed348025db62735af37bbb5bdcef6e6853a8
Ron Dagdag <ron@spacee.com>
2021-09-11 19:45:51 -0500

    initial blink

--- PerceptEdgeSolution/modules/ArduinoModule/Program.cs
+++ PerceptEdgeSolution/modules/ArduinoModule/Program.cs
@@ -14,15 +14,23 @@ namespace ArduinoModule
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
     using Microsoft.Extensions.Logging;
+    using System.Device.Gpio;
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+    using System.Linq;
 
     class Program
     {
         static int counter;
+        static ArduinoBoard board;
+        static GpioController gpioController;
+        // Use Pin 6
+        const int gpio = 6;
 
         static void Main(string[] args)
         {
 
-            string portName = "/dev/ttyS3";
+            string portName = "/dev/ttyACM0";
 
             var loggerFactory = LoggerFactory.Create(builder =>
             {
@@ -45,13 +53,21 @@ namespace ArduinoModule
                     return;
                 }
 
-                ArduinoBoard board = new ArduinoBoard(port.BaseStream);
+                board = new ArduinoBoard(port.BaseStream);
                 try
                 {
                     // This implicitly connects
                     Console.WriteLine($"Connecting... Firmware version: {board.FirmwareVersion}, Builder: {board.FirmwareName}");
                     //while (Menu(board))
+                    while (true)
                     {
+                        Init().Wait();
+
+                        // Wait until the app unloads or is cancelled
+                        var cts = new CancellationTokenSource();
+                        AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
+                        Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
+                        WhenCancelled(cts.Token).Wait();
                     }
                 }
                 catch (TimeoutException x)
@@ -64,14 +80,6 @@ namespace ArduinoModule
                     board?.Dispose();
                 }
             }
-            
-            Init().Wait();
-
-            // Wait until the app unloads or is cancelled
-            var cts = new CancellationTokenSource();
-            AssemblyLoadContext.Default.Unloading += (ctx) => cts.Cancel();
-            Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
-            WhenCancelled(cts.Token).Wait();
         }
 
         /// <summary>
@@ -97,9 +105,11 @@ namespace ArduinoModule
             ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
             await ioTHubModuleClient.OpenAsync();
             Console.WriteLine("IoT Hub module client initialized.");
-
+            
             // Register callback to be called when a message is received by the module
             await ioTHubModuleClient.SetInputMessageHandlerAsync("input1", PipeMessage, ioTHubModuleClient);
+            
+            await TestGpio(board);
         }
 
         /// <summary>
@@ -122,19 +132,71 @@ namespace ArduinoModule
             Console.WriteLine($"Received message: {counterValue}, Body: [{messageString}]");
 
             if (!string.IsNullOrEmpty(messageString))
-            {
-                using (var pipeMessage = new Message(messageBytes))
-                {
-                    foreach (var prop in message.Properties)
-                    {
-                        pipeMessage.Properties.Add(prop.Key, prop.Value);
-                    }
-                    await moduleClient.SendEventAsync("output1", pipeMessage);
+            {                
+                // Get the body of the message and deserialize it.
+                var messageBody = JsonConvert.DeserializeObject<MessageBody>(messageString);
                 
-                    Console.WriteLine("Received message sent");
+                if (messageBody != null && messageBody.NEURAL_NETWORK != null && messageBody.NEURAL_NETWORK.Any())
+                {
+                    await Blink(gpio);
+                    
+                    using (var pipeMessage = new Message(messageBytes))
+                    {
+                        foreach (var prop in message.Properties)
+                        {
+                            pipeMessage.Properties.Add(prop.Key, prop.Value);
+                        }
+                        await moduleClient.SendEventAsync("output1", pipeMessage);
+                    
+                        Console.WriteLine("Received message sent");
+                    }
                 }
             }
             return MessageResponse.Completed;
         }
+
+        public static async Task TestGpio(ArduinoBoard board)
+        {            
+            gpioController = board.CreateGpioController();
+
+            // Opening GPIO2
+            gpioController.OpenPin(gpio);
+            gpioController.SetPinMode(gpio, PinMode.Output);
+
+            Console.WriteLine("Blinking GPIO6");
+            //while (!Console.KeyAvailable)
+            //while (true)
+            {
+                await Blink(gpio);
+                //Thread.Sleep(500);
+            }
+
+            //Console.ReadKey();
+            //gpioController.Dispose();
+        }
+
+        private static async Task Blink(int gpio)
+        {
+            Console.WriteLine("blink");
+            gpioController.Write(gpio, PinValue.High);
+            //Thread.Sleep(500);
+            await Task.Delay(500);
+            gpioController.Write(gpio, PinValue.Low);
+            await Task.Delay(500);
+        }
+    }
+
+        //Define the expected schema for the body of incoming messages.
+    public class NEURALNETWORK
+    {
+        public List<double> bbox { get; set; }
+        public string label { get; set; }
+        public string confidence { get; set; }
+        public string timestamp { get; set; }
+    }
+
+    public class MessageBody
+    {
+        public List<NEURALNETWORK> NEURAL_NETWORK { get; set; }
     }
 }
